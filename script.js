c1a0221852624c320182bfec1b849d98f494b22b
Ravi Arora <46346108+PhantomzBack@users.noreply.github.com>
2021-05-26 22:03:13 +0530

    Added GH-pages files

---
+++ script.js
@@ -0,0 +1,15 @@
+function Courses(){
+  window.location="https://besttenreview.com/how-to-get-better-at-fortnite/#:~:text=%231%3A%20The%20Basics%20of%20Getting%20Better%20at%20Fortnite,the%20battle%20bus%20and%20get%20ready%20to%20drop."
+}
+function Books(){
+  window.location="https://besttenreview.com/how-to-get-better-at-fortnite/#:~:text=%231%3A%20The%20Basics%20of%20Getting%20Better%20at%20Fortnite,the%20battle%20bus%20and%20get%20ready%20to%20drop."
+}
+function About(){
+  window.location="https://besttenreview.com/how-to-get-better-at-fortnite/#:~:text=%231%3A%20The%20Basics%20of%20Getting%20Better%20at%20Fortnite,the%20battle%20bus%20and%20get%20ready%20to%20drop."
+}
+function calculator(){
+   window.location="https://besttenreview.com/how-to-get-better-at-fortnite/#:~:text=%231%3A%20The%20Basics%20of%20Getting%20Better%20at%20Fortnite,the%20battle%20bus%20and%20get%20ready%20to%20drop."
+}
+function terms(){
+  window.location="https://besttenreview.com/how-to-get-better-at-fortnite/#:~:text=%231%3A%20The%20Basics%20of%20Getting%20Better%20at%20Fortnite,the%20battle%20bus%20and%20get%20ready%20to%20drop."
+}
